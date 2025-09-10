ba675c5204385289113cf6876bb647b2ae5b2117
Thomas bH. Ptacek <thomas@sockpuppet.org>
2025-09-10 18:40:29 -0500

    template for tool runner function

--- toolconfig.go
+++ toolconfig.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 
 	"github.com/BurntSushi/toml"
+	"github.com/superfly/contextwindow"
 )
 
 type ToolParameter struct {
@@ -49,4 +52,65 @@ func LoadToolConfig(configPath string) (*ToolsConfig, error) {
 	}
 
 	return &config, nil
+}
+
+type simpleToolFunction func(context.Context, json.RawMessage) (string, error)
+
+func generateCommand(
+	cmd string,
+	params map[string]ToolParameter) simpleToolFunction {
+
+	return func(ctx context.Context, args json.RawMessage) (string, error) {
+		// assume we have parameters:
+		//   query (string)
+		//   limit (number)
+		//
+		// then our JSON message will look like:
+		//
+		// var params struct {
+		//   Query string `json:"query"`
+		//   Limit int `json:"limit"`
+		// }
+		//
+		// but we're generating this dynamically so we'll instead
+		// need to parse this out of a map[string]interface{}
+		//
+		// var params = map[string]interface{}
+		// json.Unmarshall(args, &params)
+		//
+		// then, whatever "cmd" is, we'll need to os/exec it,
+		// substituting in {query} and {limit} into the string
+		// we execute.
+		//
+		// the command will produce a string (or an error);
+		// we can return both directly.
+
+		return "", nil /* placeholder */
+	}
+}
+
+func LoadTools(cw *contextwindow.ContextWindow, cfg *ToolsConfig) error {
+	for _, toolCfg := range cfg.Tools {
+		tool := contextwindow.NewTool(
+			toolCfg.Name,
+			toolCfg.Description)
+		for pk, pv := range toolCfg.Parameters {
+			switch pv.Type {
+			case "string":
+				tool = tool.AddStringParameter(pk, pv.Description, pv.Required)
+			case "number":
+				tool = tool.AddNumberParameter(pk, pv.Description, pv.Required)
+			default:
+				return fmt.Errorf("load tools: unknown parameter type \"%s\" for %s",
+					pv.Type, toolCfg.Name)
+			}
+		}
+
+		cw.AddTool(tool,
+			contextwindow.ToolRunnerFunc(
+				generateCommand(toolCfg.Name, toolCfg.Parameters),
+			))
+	}
+
+	return nil
 }
